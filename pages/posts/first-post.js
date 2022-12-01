b0a643e7ca880ced09acafd1e27537b1ac09e421
jdssanil <sanilk@jdsindustries.com>
2022-12-01 16:49:20 -0600

    tutorial code

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -10,9 +10,6 @@ export default function FirstPost() {
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">← Back to home</Link>
-      </h2>
     </Layout>
   );
 }
